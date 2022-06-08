e709b2757cffcedc452bc8d54e3363b783067fc5
Heydi <sucelyalvarez@hotmail.com>
2022-06-08 14:22:02 -0600

    Mantenimientos Solicitados

---
+++ ExamenFinal/CapaVista/frmBodega.Designer.cs
@@ -0,0 +1,217 @@
+﻿
+namespace CapaVista
+{
+	partial class frmBodega
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBodega));
+			this.txtEstado = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.rbnEstatusamodulo = new System.Windows.Forms.RadioButton();
+			this.rbnEstatusimodulo = new System.Windows.Forms.RadioButton();
+			this.lblEstatus = new System.Windows.Forms.Label();
+			this.txtDireccion = new System.Windows.Forms.TextBox();
+			this.txtNombre = new System.Windows.Forms.TextBox();
+			this.txtID = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.navegador1 = new DLL.nav.navegador();
+			this.dgvVistaPrevia = new System.Windows.Forms.DataGridView();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvVistaPrevia)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// txtEstado
+			// 
+			this.txtEstado.Location = new System.Drawing.Point(342, 209);
+			this.txtEstado.Name = "txtEstado";
+			this.txtEstado.Size = new System.Drawing.Size(33, 20);
+			this.txtEstado.TabIndex = 100;
+			this.txtEstado.TabStop = false;
+			this.txtEstado.Tag = "estado";
+			this.txtEstado.Visible = false;
+			this.txtEstado.TextChanged += new System.EventHandler(this.txtEstado_TextChanged);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.rbnEstatusamodulo);
+			this.panel1.Controls.Add(this.rbnEstatusimodulo);
+			this.panel1.Location = new System.Drawing.Point(136, 199);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 30);
+			this.panel1.TabIndex = 101;
+			// 
+			// rbnEstatusamodulo
+			// 
+			this.rbnEstatusamodulo.AutoSize = true;
+			this.rbnEstatusamodulo.Location = new System.Drawing.Point(12, 7);
+			this.rbnEstatusamodulo.Name = "rbnEstatusamodulo";
+			this.rbnEstatusamodulo.Size = new System.Drawing.Size(55, 17);
+			this.rbnEstatusamodulo.TabIndex = 6;
+			this.rbnEstatusamodulo.TabStop = true;
+			this.rbnEstatusamodulo.Text = "Activo";
+			this.rbnEstatusamodulo.UseVisualStyleBackColor = true;
+			this.rbnEstatusamodulo.CheckedChanged += new System.EventHandler(this.rbnEstatusamodulo_CheckedChanged);
+			// 
+			// rbnEstatusimodulo
+			// 
+			this.rbnEstatusimodulo.AutoSize = true;
+			this.rbnEstatusimodulo.Location = new System.Drawing.Point(134, 7);
+			this.rbnEstatusimodulo.Name = "rbnEstatusimodulo";
+			this.rbnEstatusimodulo.Size = new System.Drawing.Size(63, 17);
+			this.rbnEstatusimodulo.TabIndex = 7;
+			this.rbnEstatusimodulo.TabStop = true;
+			this.rbnEstatusimodulo.Text = "Inactivo";
+			this.rbnEstatusimodulo.UseVisualStyleBackColor = true;
+			this.rbnEstatusimodulo.CheckedChanged += new System.EventHandler(this.rbnEstatusimodulo_CheckedChanged);
+			// 
+			// lblEstatus
+			// 
+			this.lblEstatus.AutoSize = true;
+			this.lblEstatus.Location = new System.Drawing.Point(56, 202);
+			this.lblEstatus.Name = "lblEstatus";
+			this.lblEstatus.Size = new System.Drawing.Size(45, 13);
+			this.lblEstatus.TabIndex = 99;
+			this.lblEstatus.Text = "Estatus:";
+			// 
+			// txtDireccion
+			// 
+			this.txtDireccion.Location = new System.Drawing.Point(136, 162);
+			this.txtDireccion.Name = "txtDireccion";
+			this.txtDireccion.Size = new System.Drawing.Size(209, 20);
+			this.txtDireccion.TabIndex = 95;
+			this.txtDireccion.Tag = "direccion";
+			// 
+			// txtNombre
+			// 
+			this.txtNombre.Location = new System.Drawing.Point(136, 136);
+			this.txtNombre.Name = "txtNombre";
+			this.txtNombre.Size = new System.Drawing.Size(209, 20);
+			this.txtNombre.TabIndex = 94;
+			this.txtNombre.Tag = "nombre";
+			// 
+			// txtID
+			// 
+			this.txtID.Location = new System.Drawing.Point(136, 113);
+			this.txtID.Name = "txtID";
+			this.txtID.Size = new System.Drawing.Size(209, 20);
+			this.txtID.TabIndex = 93;
+			this.txtID.Tag = "Pkid";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(49, 172);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(52, 13);
+			this.label3.TabIndex = 98;
+			this.label3.Text = "Dirección";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(49, 139);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(44, 13);
+			this.label2.TabIndex = 97;
+			this.label2.Text = "Nombre";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(49, 113);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(18, 13);
+			this.label1.TabIndex = 96;
+			this.label1.Text = "ID";
+			// 
+			// navegador1
+			// 
+			this.navegador1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+			this.navegador1.Location = new System.Drawing.Point(-3, -3);
+			this.navegador1.Name = "navegador1";
+			this.navegador1.Size = new System.Drawing.Size(1189, 87);
+			this.navegador1.TabIndex = 92;
+			// 
+			// dgvVistaPrevia
+			// 
+			this.dgvVistaPrevia.AllowUserToAddRows = false;
+			this.dgvVistaPrevia.AllowUserToDeleteRows = false;
+			this.dgvVistaPrevia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvVistaPrevia.Location = new System.Drawing.Point(403, 90);
+			this.dgvVistaPrevia.Name = "dgvVistaPrevia";
+			this.dgvVistaPrevia.ReadOnly = true;
+			this.dgvVistaPrevia.RowHeadersWidth = 51;
+			this.dgvVistaPrevia.Size = new System.Drawing.Size(769, 179);
+			this.dgvVistaPrevia.TabIndex = 102;
+			// 
+			// frmBodega
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1184, 298);
+			this.Controls.Add(this.dgvVistaPrevia);
+			this.Controls.Add(this.txtEstado);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.lblEstatus);
+			this.Controls.Add(this.txtDireccion);
+			this.Controls.Add(this.txtNombre);
+			this.Controls.Add(this.txtID);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.navegador1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "frmBodega";
+			this.Text = "Bodega";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvVistaPrevia)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtEstado;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.RadioButton rbnEstatusamodulo;
+		private System.Windows.Forms.RadioButton rbnEstatusimodulo;
+		private System.Windows.Forms.Label lblEstatus;
+		private System.Windows.Forms.TextBox txtDireccion;
+		private System.Windows.Forms.TextBox txtNombre;
+		private System.Windows.Forms.TextBox txtID;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private DLL.nav.navegador navegador1;
+		private System.Windows.Forms.DataGridView dgvVistaPrevia;
+	}
+}
---
+++ ExamenFinal/CapaVista/frmCliente.Designer.cs
@@ -0,0 +1,263 @@
+﻿
+namespace CapaVista
+{
+	partial class frmCliente
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCliente));
+			this.txtEstado = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.rbnEstatusamodulo = new System.Windows.Forms.RadioButton();
+			this.rbnEstatusimodulo = new System.Windows.Forms.RadioButton();
+			this.lblEstatus = new System.Windows.Forms.Label();
+			this.dgvVistaPrevia = new System.Windows.Forms.DataGridView();
+			this.txtDireccion = new System.Windows.Forms.TextBox();
+			this.txtNIT = new System.Windows.Forms.TextBox();
+			this.txtNombre = new System.Windows.Forms.TextBox();
+			this.txtID = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.navegador1 = new DLL.nav.navegador();
+			this.txtTelefono = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvVistaPrevia)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// txtEstado
+			// 
+			this.txtEstado.Location = new System.Drawing.Point(399, 241);
+			this.txtEstado.Name = "txtEstado";
+			this.txtEstado.Size = new System.Drawing.Size(33, 20);
+			this.txtEstado.TabIndex = 103;
+			this.txtEstado.TabStop = false;
+			this.txtEstado.Tag = "estado";
+			this.txtEstado.Visible = false;
+			this.txtEstado.TextChanged += new System.EventHandler(this.txtEstado_TextChanged);
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.rbnEstatusamodulo);
+			this.panel1.Controls.Add(this.rbnEstatusimodulo);
+			this.panel1.Location = new System.Drawing.Point(161, 241);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(200, 30);
+			this.panel1.TabIndex = 104;
+			// 
+			// rbnEstatusamodulo
+			// 
+			this.rbnEstatusamodulo.AutoSize = true;
+			this.rbnEstatusamodulo.Location = new System.Drawing.Point(12, 7);
+			this.rbnEstatusamodulo.Name = "rbnEstatusamodulo";
+			this.rbnEstatusamodulo.Size = new System.Drawing.Size(55, 17);
+			this.rbnEstatusamodulo.TabIndex = 6;
+			this.rbnEstatusamodulo.TabStop = true;
+			this.rbnEstatusamodulo.Text = "Activo";
+			this.rbnEstatusamodulo.UseVisualStyleBackColor = true;
+			this.rbnEstatusamodulo.CheckedChanged += new System.EventHandler(this.rbnEstatusamodulo_CheckedChanged);
+			// 
+			// rbnEstatusimodulo
+			// 
+			this.rbnEstatusimodulo.AutoSize = true;
+			this.rbnEstatusimodulo.Location = new System.Drawing.Point(134, 7);
+			this.rbnEstatusimodulo.Name = "rbnEstatusimodulo";
+			this.rbnEstatusimodulo.Size = new System.Drawing.Size(63, 17);
+			this.rbnEstatusimodulo.TabIndex = 7;
+			this.rbnEstatusimodulo.TabStop = true;
+			this.rbnEstatusimodulo.Text = "Inactivo";
+			this.rbnEstatusimodulo.UseVisualStyleBackColor = true;
+			this.rbnEstatusimodulo.CheckedChanged += new System.EventHandler(this.rbnEstatusimodulo_CheckedChanged);
+			// 
+			// lblEstatus
+			// 
+			this.lblEstatus.AutoSize = true;
+			this.lblEstatus.Location = new System.Drawing.Point(51, 244);
+			this.lblEstatus.Name = "lblEstatus";
+			this.lblEstatus.Size = new System.Drawing.Size(45, 13);
+			this.lblEstatus.TabIndex = 102;
+			this.lblEstatus.Text = "Estatus:";
+			// 
+			// dgvVistaPrevia
+			// 
+			this.dgvVistaPrevia.AllowUserToAddRows = false;
+			this.dgvVistaPrevia.AllowUserToDeleteRows = false;
+			this.dgvVistaPrevia.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dgvVistaPrevia.Location = new System.Drawing.Point(438, 89);
+			this.dgvVistaPrevia.Name = "dgvVistaPrevia";
+			this.dgvVistaPrevia.ReadOnly = true;
+			this.dgvVistaPrevia.RowHeadersWidth = 51;
+			this.dgvVistaPrevia.Size = new System.Drawing.Size(736, 204);
+			this.dgvVistaPrevia.TabIndex = 101;
+			// 
+			// txtDireccion
+			// 
+			this.txtDireccion.Location = new System.Drawing.Point(137, 189);
+			this.txtDireccion.Name = "txtDireccion";
+			this.txtDireccion.Size = new System.Drawing.Size(209, 20);
+			this.txtDireccion.TabIndex = 96;
+			this.txtDireccion.Tag = "direccion";
+			// 
+			// txtNIT
+			// 
+			this.txtNIT.Location = new System.Drawing.Point(137, 161);
+			this.txtNIT.Name = "txtNIT";
+			this.txtNIT.Size = new System.Drawing.Size(209, 20);
+			this.txtNIT.TabIndex = 95;
+			this.txtNIT.Tag = "nit";
+			// 
+			// txtNombre
+			// 
+			this.txtNombre.Location = new System.Drawing.Point(137, 135);
+			this.txtNombre.Name = "txtNombre";
+			this.txtNombre.Size = new System.Drawing.Size(209, 20);
+			this.txtNombre.TabIndex = 94;
+			this.txtNombre.Tag = "nombre";
+			// 
+			// txtID
+			// 
+			this.txtID.Location = new System.Drawing.Point(137, 112);
+			this.txtID.Name = "txtID";
+			this.txtID.Size = new System.Drawing.Size(209, 20);
+			this.txtID.TabIndex = 93;
+			this.txtID.Tag = "Pkid";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(50, 192);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(52, 13);
+			this.label4.TabIndex = 100;
+			this.label4.Text = "Direccion";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(50, 164);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(25, 13);
+			this.label3.TabIndex = 99;
+			this.label3.Text = "NIT";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(50, 138);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(44, 13);
+			this.label2.TabIndex = 98;
+			this.label2.Text = "Nombre";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(50, 112);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(18, 13);
+			this.label1.TabIndex = 97;
+			this.label1.Text = "ID";
+			// 
+			// navegador1
+			// 
+			this.navegador1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(248)))), ((int)(((byte)(248)))));
+			this.navegador1.Location = new System.Drawing.Point(-2, -4);
+			this.navegador1.Name = "navegador1";
+			this.navegador1.Size = new System.Drawing.Size(1189, 87);
+			this.navegador1.TabIndex = 92;
+			// 
+			// txtTelefono
+			// 
+			this.txtTelefono.Location = new System.Drawing.Point(137, 215);
+			this.txtTelefono.Name = "txtTelefono";
+			this.txtTelefono.Size = new System.Drawing.Size(209, 20);
+			this.txtTelefono.TabIndex = 105;
+			this.txtTelefono.Tag = "telefono";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(50, 218);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(49, 13);
+			this.label5.TabIndex = 106;
+			this.label5.Text = "Telefono";
+			// 
+			// frmCliente
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1186, 304);
+			this.Controls.Add(this.txtEstado);
+			this.Controls.Add(this.txtTelefono);
+			this.Controls.Add(this.txtDireccion);
+			this.Controls.Add(this.txtNIT);
+			this.Controls.Add(this.txtNombre);
+			this.Controls.Add(this.txtID);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.lblEstatus);
+			this.Controls.Add(this.dgvVistaPrevia);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.navegador1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "frmCliente";
+			this.Text = "Cliente";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dgvVistaPrevia)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtEstado;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.RadioButton rbnEstatusamodulo;
+		private System.Windows.Forms.RadioButton rbnEstatusimodulo;
+		private System.Windows.Forms.Label lblEstatus;
+		private System.Windows.Forms.DataGridView dgvVistaPrevia;
+		private System.Windows.Forms.TextBox txtDireccion;
+		private System.Windows.Forms.TextBox txtNIT;
+		private System.Windows.Forms.TextBox txtNombre;
+		private System.Windows.Forms.TextBox txtID;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private DLL.nav.navegador navegador1;
+		private System.Windows.Forms.TextBox txtTelefono;
+		private System.Windows.Forms.Label label5;
+	}
+}
